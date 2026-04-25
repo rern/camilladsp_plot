2e91d6679668572a09569dcfe40388e7695d375a
rern <rernrern@gmail.com>
2026-04-25 20:00:01 +0700

    Update camilladsp_plot.js

--- srv/http/assets/js/plugin/camilladsp_plot.js
+++ srv/http/assets/js/plugin/camilladsp_plot.js
@@ -1,16 +1,3 @@
-/*
-convert from https://github.com/HEnquist/pycamilladsp-plot/blob/master/camilladsp_plot/
-this file - camilladsp_plot.js << filters.py
-	- require complex.js - math operations >> serial of complex function
-		- Complex( 0, 1 )
-			.mul( 2 * Math.PI * f / this.fs )
-			.exp()
-	- add fft() for Conv complexGain()
-camilla.js:
-	- graph.filter(), graph.pipeline() << eval_filterconfig.py
-camilla_audiofilread.py << audiofilread.py
-	- modify for graph.filter() - Conv
-*/
 function calcGroupDelay( freq, phase ) {
 	if ( freq.length < 2 ) {
 		return [ [], [] ];
